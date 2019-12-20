eb4292d00003bf2b2569a7e625a9006ca27e6ff5
Jeremiah Thuku <thuku8@gmail.com>
2019-12-20 16:32:06 +0300

    started on splash screen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget{
+	@override
+	Widget build(BuildContext context) {
+		return Scaffold(
+			body: Center(
+				child: Text(
+					"Splash Screen",
+					style: TextStyle(
+						color: Colors.black54,
+					),
+				),
+			)
+		);
+	}
+}
