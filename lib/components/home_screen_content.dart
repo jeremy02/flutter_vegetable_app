90ad4c84c07ad3c47b4fef6c46781bd4c7ad4269
Jeremiah Thuku <thuku8@gmail.com>
2019-12-23 19:33:58 +0300

    added grid cards

--- lib/components/home_screen_content.dart
+++ lib/components/home_screen_content.dart
@@ -31,8 +31,37 @@ class HomeScreenContent extends StatelessWidget{
 										right: 16,
 										left: 16,
 									),
-									decoration: BoxDecoration(
-										color: Colors.orange
+									child: GridView.builder(
+										physics: AlwaysScrollableScrollPhysics(),
+										shrinkWrap: true,
+										gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+											crossAxisCount: 2,
+											crossAxisSpacing: 0.0,
+											mainAxisSpacing: 0.0,
+										),
+										itemCount: 10,
+										itemBuilder: (context, index) {
+											return Padding(
+												padding: EdgeInsets.only(
+													top: 0.0,
+													bottom: 12.0,
+													left: index % 2 == 0 ? 0.0 : 8.0,
+												),
+												child: Card(
+													shape: RoundedRectangleBorder(
+														borderRadius: BorderRadius.all(Radius.circular(16.0)),
+													),
+													child: InkWell(
+														borderRadius: BorderRadius.all(Radius.circular(16)),
+														onTap: (){
+														
+														},
+													),
+													elevation: 12.0,
+													color: Colors.white,
+												),
+											);
+										},
 									),
 								),
 							),
