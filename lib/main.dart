697126bdaa94df491f7ddef339f8fdb3ca20a0e2
Jeremiah Thuku <thuku8@gmail.com>
2019-12-23 11:38:26 +0300

    added the font

--- lib/main.dart
+++ lib/main.dart
@@ -3,27 +3,13 @@ import 'package:flutter/services.dart';
 import 'package:flutter_vegetable_app/screens/splash_screen.dart';
 
 void main() async{
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then((_) {
-    runApp(MyApp());
-  });
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then((_) {
+        runApp(MyApp());
+    });
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
     @override
-    void initState() {
-        super.initState();
-        SystemChrome.setEnabledSystemUIOverlays(SystemUiOverlay.values);
-        SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-            statusBarColor: Color.fromRGBO(235, 74, 66, 1.0),
-            statusBarIconBrightness: Brightness.dark,
-        ));
-  }
-  
     @override
     Widget build(BuildContext context) {
         return MaterialApp(
@@ -32,6 +18,7 @@ class _MyAppState extends State<MyApp> {
             theme: ThemeData(
                 // This is the theme of you
                 primarySwatch: Colors.red,
+                fontFamily: 'NTSAkkhara',
             ),
             home: SplashScreen(),
         );
