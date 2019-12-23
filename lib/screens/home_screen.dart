aecd5375ead356eb88036cbd418393ebede5468d
Jeremiah Thuku <thuku8@gmail.com>
2019-12-23 17:07:53 +0300

    finished with top arcs

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -20,11 +20,46 @@ class HomeScreen extends StatelessWidget{
 									Align(
 										alignment: Alignment.topLeft,
 										child: ClipPath(
-											clipper: TopLeftSplashScreenArc(),
+											clipper: TopLeftArc(),
 											child: Container(
-												height: MediaQuery.of(context).size.height/4,
+												height: MediaQuery.of(context).size.height/3,
 												decoration: BoxDecoration(
 													color: Color.fromRGBO(255, 209, 50, 1.0),
+												),
+											),
+										),
+									),
+									Align(
+										alignment: Alignment.topRight,
+										child: ClipPath(
+											clipper: TopRightArc(),
+											child: Container(
+												height: MediaQuery.of(context).size.height/3,
+												decoration: BoxDecoration(
+													color: Color.fromRGBO(235, 74, 66, 1.0),
+												),
+												child: LayoutBuilder(
+													builder: (BuildContext context, BoxConstraints viewportConstraints){
+														return Stack(
+															children: <Widget>[
+																Align(
+																	alignment: Alignment.topCenter,
+																	child: Padding(
+																		padding: EdgeInsets.only(top: viewportConstraints.minHeight/3),
+																		child: Text(
+																			"Vegetable",
+																			style: TextStyle(
+																				fontSize: 20.0,
+																				color: Colors.white,
+																				fontWeight: FontWeight.bold,
+																				letterSpacing: 1.2,
+																			),
+																		),
+																	)
+																),
+															],
+														);
+													},
 												),
 											),
 										),
@@ -47,7 +82,7 @@ class HomeScreen extends StatelessWidget{
 	}
 }
 
-class TopLeftSplashScreenArc extends CustomClipper<Path>{
+class TopLeftArc extends CustomClipper<Path>{
 	
 	@override
 	Path getClip(Size size) {
@@ -56,7 +91,7 @@ class TopLeftSplashScreenArc extends CustomClipper<Path>{
 		path.lineTo(0.0, size.height);
 		
 		//move from bottom right to top right - height
-		var firstControlPoint = Offset(size.width/2, size.height/3);
+		var firstControlPoint = Offset(size.width/2.5, size.height/2);
 		var firstEndPoint = Offset(size.width, size.height/2);
 		path.quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy,
 			firstEndPoint.dx, firstEndPoint.dy);
@@ -65,7 +100,33 @@ class TopLeftSplashScreenArc extends CustomClipper<Path>{
 		path.lineTo(size.width, 0.0);
 		
 		path.close();
+		return path;
+	}
+	
+	@override
+	bool shouldReclip(CustomClipper<Path> oldClipper) {
+		return true;
+	}
+}
+
+class TopRightArc extends CustomClipper<Path>{
+	
+	@override
+	Path getClip(Size size) {
+		Path path = new Path();
 		
+		path.lineTo(0.0, size.height/2);
+		
+		//move to bottom right
+		var firstControlPoint = Offset(size.width/1.5, size.height/2);
+		var firstEndPoint = Offset(size.width, size.height);
+		path.quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy,
+			firstEndPoint.dx, firstEndPoint.dy);
+		
+		///move to top right
+		path.lineTo(size.width, 0.0);
+		
+		path.close();
 		return path;
 	}
 	
