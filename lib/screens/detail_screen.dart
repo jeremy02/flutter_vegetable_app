9ae719f59a141186f51df581f886d5b39a6ad3b5
Jeremiah Thuku <thuku8@gmail.com>
2020-01-02 15:07:38 +0300

    detail content added

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -37,7 +37,7 @@ class DetailScreen extends StatelessWidget{
 								)
 							),
 							Flexible(
-								flex: 2,
+								flex: 1,
 								child: Padding(
 									padding: EdgeInsets.symmetric(horizontal: 26.0),
 									child: Stack(
@@ -49,57 +49,50 @@ class DetailScreen extends StatelessWidget{
 												child: FractionallySizedBox(
 													widthFactor: 1.0,
 													heightFactor: 0.94,
-													child: Container(
-														decoration: BoxDecoration(
-															color: Colors.white,
-															borderRadius: BorderRadius.all(Radius.circular(20.0)),
-														),
+													child: LayoutBuilder(
+														builder: (context, constraints) {
+															return Column(
+																children: <Widget>[
+																	SizedBox(height: 16.0,),
+																	Image.asset(
+																		"assets/images/fresh_onion.jpg",
+																		fit: BoxFit.contain,
+																		height: constraints.maxHeight*0.50,
+																	),
+																	SizedBox(height: 8.0,),
+																	Expanded(
+																		child: Padding(
+																			padding: EdgeInsets.symmetric(
+																				horizontal: 10.0,
+																			),
+																			child: Column(
+																				children: <Widget>[
+																					  titleContent(),
+																					  descriptionContent(),
+																				],
+																			),
+																		),
+																	),
+																	SizedBox(height: 16.0,),
+																],
+															);
+														}
 													),
 												),
 											),
 											Align(
 												alignment: Alignment.bottomCenter,
-												child: InkWell(
-													splashColor: Colors.black.withOpacity(0.4),
-													onTap: (){},
-													child: Builder(
-														builder: (BuildContext context) {
-															return RaisedButton(
-																textColor: Colors.white,
-																color: Color.fromRGBO(136, 187, 19, 1.0),
-																child: Text(
-																	"Add to Cart",
-																	style: TextStyle(
-																		fontWeight: FontWeight.bold,
-																	),
-																),
-																padding: EdgeInsets.symmetric(
-																	vertical: 12.0,
-																	horizontal: 20.0,
-																),
-																onPressed: () {
-																	// add to cart action
-																	addToCart(context);
-																},
-																shape: RoundedRectangleBorder(
-																	borderRadius: BorderRadius.circular(30.0),
-																),
-															);
-														},
-													),
-												),
+												child: addToCartButton(context),
 											),
 										],
 									),
 								),
 							),
-							Flexible(
-								flex: 1,
-								child: Container(
-									margin: EdgeInsets.all(16.0),
-									decoration: BoxDecoration(
-										color: Colors.red,
-									),
+							Container(
+								height: MediaQuery.of(context).size.height*0.18,
+								margin: EdgeInsets.all(10.0),
+								decoration: BoxDecoration(
+									color: Colors.red,
 								),
 							),
 						],
@@ -109,20 +102,111 @@ class DetailScreen extends StatelessWidget{
 		);
 	}
 
-    addToCart(BuildContext context) {
-	    // show snackbar for adding to cart
-	    final snackBar = SnackBar(
-		    content: Text('Added To Cart!'),
-		    action: SnackBarAction(
-			    label: 'REMOVE',
-			    onPressed: () {
-				    // Some code to undo the change.
-			    },
-		    ),
-	    );
-	
-	    // Find the Scaffold in the widget tree and use
-	    // it to show a SnackBar.
-	    Scaffold.of(context).showSnackBar(snackBar);
+	// the add to cart button
+    Widget addToCartButton(BuildContext context) {
+		return InkWell(
+			splashColor: Colors.black.withOpacity(0.4),
+			onTap: (){},
+			child: Builder(
+				builder: (BuildContext context) {
+					return RaisedButton(
+						textColor: Colors.white,
+						color: Color.fromRGBO(136, 187, 19, 1.0),
+						child: Text(
+							"Add to Cart",
+							style: TextStyle(
+								fontWeight: FontWeight.bold,
+							),
+						),
+						padding: EdgeInsets.symmetric(
+							vertical: 12.0,
+							horizontal: 20.0,
+						),
+						onPressed: () {
+							// add to cart action
+							addToCart(context);
+						},
+						shape: RoundedRectangleBorder(
+							borderRadius: BorderRadius.circular(30.0),
+						),
+					);
+				},
+			),
+		);
     }
+	
+	// the add to cart button function
+	addToCart(BuildContext context) {
+		// show snackbar for adding to cart
+		final snackBar = SnackBar(
+			content: Text('Added To Cart!'),
+			action: SnackBarAction(
+				label: 'REMOVE',
+				onPressed: () {
+					// Some code to undo the change.
+				},
+			),
+		);
+		
+		// Find the Scaffold in the widget tree and use
+		// it to show a SnackBar.
+		Scaffold.of(context).showSnackBar(snackBar);
+	}
+
+    Widget titleContent() {
+		return Row(
+			mainAxisAlignment: MainAxisAlignment.spaceBetween,
+			children: <Widget>[
+				Column(
+					crossAxisAlignment: CrossAxisAlignment.start,
+					children: <Widget>[
+						Text(
+							"Red Capsicum",
+							style: TextStyle(
+								fontWeight: FontWeight.bold,
+								color: Colors.black,
+								fontSize: 18.0,
+							),
+						),
+						Padding(
+							padding: EdgeInsets.only(top: 8.0),
+							child: Text(
+								"900gm",
+								style: TextStyle(
+									fontWeight: FontWeight.bold,
+									color: Color.fromRGBO(185, 184, 184, 1.0),
+									fontSize: 14.0,
+								),
+							),
+						),
+					],
+				),
+				Text(
+					"\$18",
+					style: TextStyle(
+						fontWeight: FontWeight.bold,
+						color: Colors.black,
+						fontSize: 20.0,
+					),
+				),
+			],
+		);
+    }
+	
+	Widget descriptionContent() {
+		return Padding(
+			padding: EdgeInsets.only(top: 8.0),
+			child: Text(
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
+					"Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in "
+					"reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in "
+					"culpa qui officia deserunt mollit anim id est laborum.",
+				style: TextStyle(
+					fontWeight: FontWeight.normal,
+					color: Color.fromRGBO(227, 226, 226, 1.0),
+					fontSize: 14.0,
+				),
+			),
+		);
+	}
 }
